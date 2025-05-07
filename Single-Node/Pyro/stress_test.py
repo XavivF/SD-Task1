7493fd1d7f8c5b747047a3c4163eda9bcb10a3ca
Xavier Vilanova Fuambuena <xavier.vilanova@estudiants.urv.cat>
2025-05-07 09:21:25 +0200

    Update stress_test.py

--- Single-Node/Pyro/stress_test.py
+++ Single-Node/Pyro/stress_test.py
@@ -72,10 +72,10 @@ def worker_filter_text(pyro_name, results_queue, end_time):
     results_queue.put((local_request_count, local_error_count))
 
 # --- Main function ---
-def run_stress_test(mode, pyro_name, duration, concurrency):
-    if (mode == "filter_text") and (pyro_name == "pyro.service"):
-        print(f"Error: The mode '{mode}' is not compatible with the Pyro name '{pyro_name}'.", file=sys.stderr)
-        exit (1)
+def run_stress_test(mode, duration, concurrency):
+    pyro_name = ""
+    if (mode == "filter_text"): pyro_name = DEFAULT_PYRO_FILTER
+    if (mode == "add_insult"): pyro_name = DEFAULT_PYRO_SERVICE
     print(f"Starting Pyro stress test with mode '{mode}'...")
     print(f"Pyro name: {pyro_name}")
     print(f"Duration: {duration} seconds")
@@ -148,8 +148,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Stress Test Script for Pyro Insult Service")
     parser.add_argument("mode", choices=['add_insult', 'filter_text'],
                         help="The function to test ('add_insult' o 'filter_text')")
-    parser.add_argument("-n", "--name", default=DEFAULT_PYRO_SERVICE,
-                        help=f"Name of the Pyro object in the name server, - {DEFAULT_PYRO_SERVICE} - or - {DEFAULT_PYRO_FILTER} - (default: {DEFAULT_PYRO_SERVICE})")
     parser.add_argument("-d", "--duration", type=int, default=DEFAULT_DURATION,
                         help=f"Duration in seconds (default: {DEFAULT_DURATION})")
     parser.add_argument("-c", "--concurrency", type=int, default=DEFAULT_CONCURRENCY,
@@ -157,4 +155,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     # It may be necessary to start the name server manually first: python3 -m Pyro4.naming
-    run_stress_test(args.mode, args.name, args.duration, args.concurrency)
+    run_stress_test(args.mode, args.duration, args.concurrency)
